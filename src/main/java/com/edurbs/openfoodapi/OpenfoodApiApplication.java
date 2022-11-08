22c1769eddc2daa21b3c8441cb302fcfeecfcc00
Eduardo Soares <edurbs@gmail.com>
2022-11-08 11:13:31 -0300

    starting

---
+++ src/main/java/com/edurbs/openfoodapi/OpenfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.edurbs.openfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edurbs/openfoodapi/OpenfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edurbs.openfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
