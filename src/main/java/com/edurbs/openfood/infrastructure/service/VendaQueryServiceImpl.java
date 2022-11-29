aa43c89ecfc1ec9dec6e31aafb3151a3d6b88f06
Eduardo Soares <edurbs@gmail.com>
2022-11-29 10:47:46 -0300

    filtros na consulta de vendas diárias

--- src/main/java/com/edurbs/openfood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/edurbs/openfood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,21 @@
 package com.edurbs.openfood.infrastructure.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.CriteriaBuilder.In;
 
 import org.springframework.stereotype.Repository;
 
 import com.edurbs.openfood.domain.filter.VendaDiariaFilter;
 import com.edurbs.openfood.domain.model.Pedido;
+import com.edurbs.openfood.domain.model.StatusPedido;
 import com.edurbs.openfood.domain.model.dto.VendaDiaria;
 import com.edurbs.openfood.domain.service.VendaQueryService;
 
@@ -25,6 +31,20 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         var query = builder.createQuery(VendaDiaria.class);
         var root = query.from(Pedido.class);
 
+        List<Predicate> predicates = new ArrayList<>();
+        
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+        if (filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }   
+        if (filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+        
+        predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+        
         var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
         
         var selection = builder.construct(VendaDiaria.class, 
@@ -33,6 +53,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 builder.sum(root.get("valorTotal")));
         
         query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));        
         query.groupBy(functionDateDataCriacao);
 
         return manager.createQuery(query).getResultList();
