ce5f9856358390fbdc8f93e97e38fab2412a8b07
Eduardo Soares <edurbs@gmail.com>
2022-12-04 14:28:19 -0300

    feat: ShallowEtagHeaderFilter

--- src/main/java/com/edurbs/openfood/core/web/WebConfig.java
+++ src/main/java/com/edurbs/openfood/core/web/WebConfig.java
@@ -1,10 +1,14 @@
 package com.edurbs.openfood.core.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .maxAge(3600);
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter(){
+            return new ShallowEtagHeaderFilter();
     }
 
     
